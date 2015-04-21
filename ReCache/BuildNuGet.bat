8f28dee488a6f1f2f4a8fb88d14b562d7affd061
Anthony Argyrou <anthony@resqclaims.com>
2015-04-21 14:05:23 +0200

    Made changes for NuGet publishing

---
+++ ReCache/BuildNuGet.bat
@@ -0,0 +1,1 @@
+nuget pack ReCache.csproj -Prop Configuration=Release
--- ReCache/Properties/AssemblyInfo.cs
+++ ReCache/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Inivit.SuperCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ReCache")]
+[assembly: AssemblyDescription("A high performance, in-memory, concurrency safe, transparent generic key / value cache - basically, it makes some stuff a lot faster")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Inivit.SuperCache")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Jacques Bosch")]
+[assembly: AssemblyProduct("ReCache")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
