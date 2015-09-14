1b5c403e82abbe46eb9b39b99a5a0525ab84ca96
pirovorster <piro.vorster@gmail.com>
2015-09-14 21:41:52 +0200

    added async interfaces

---
+++ ReCache/IAsyncCache.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReCache
+{
+	public interface IAsyncCache<TKey, TValue> : IEnumerable<KeyValuePair<TKey, CacheEntry<TValue>>>, IDisposable
+	{
+		TValue Get(TKey key);
+
+		TValue Get(TKey key, bool resetExpiryTimeoutIfAlreadyCached);
+
+		TValue GetOrLoad(TKey key);
+
+		TValue GetOrLoad(TKey key, Func<TKey, Task<TValue>> loaderFunction);
+
+		TValue GetOrLoad(TKey key, bool resetExpiryTimeoutIfAlreadyCached);
+
+		TValue GetOrLoad(TKey key, bool resetExpiryTimeoutIfAlreadyCached, Func<TKey, Task<TValue>> loaderFunction);
+
+		bool HasKey(TKey key);
+
+		bool Invalidate(TKey key);
+
+		void InvalidateAll();
+
+		bool TryAdd(TKey key, TValue value);
+
+		Func<TKey, Task<TValue>> LoaderFunction { get; set; }
+
+		void FlushInvalidatedEntries();
+
+		int Count { get; }
+
+		IEnumerable<KeyValuePair<TKey, TValue>> Items { get; }
+
+		Action<TKey, CacheEntry<TValue>> HitCallback { get; set; }
+
+		Action<TKey, CacheEntry<TValue>, int> MissedCallback { get; set; }
+
+		Action<long, long, string, long> FlushCallback { get; set; }
+	}
+}
---
+++ ReCache/ISelfRefreshingAsyncCache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReCache
+{
+	public interface ISelfRefreshingAsyncCache<TKey, TValue> : IAsyncCache<TKey, TValue>
+	{
+		/// <summary>
+		/// Parameters are:
+		/// CurrentGeneration
+		/// DurationMilliseconds
+		/// ClientContext
+		/// </summary>
+		Action<int, int, string> RefreshCacheCallback { get; set; }
+	}
+}
